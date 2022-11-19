8c255a7a791f938b4327fd188f9fe655b1009d67
kiko7792 <josebarreirinho92@gmail.com>
2022-11-19 23:26:25 +0000

    alterações-kiko

--- ProjetoPO/src/Main/ProjetoPO.java
+++ ProjetoPO/src/Main/ProjetoPO.java
@@ -6,6 +6,7 @@ public class ProjetoPO {
     public static void main(String[] args) {
         
         System.out.println("Hello");
+        System.out.println("Está a funcionar");
     }
     
 }
