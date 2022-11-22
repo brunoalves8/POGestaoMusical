35a103b865f918335d38e99be2c1a37274d992d6
brunoalves8 <ba578731@gmail.com>
2022-11-22 19:24:50 +0000

    adicionei os construtores

---
+++ ProjetoPO/src/BackEnd/ListaProdutores.java
@@ -0,0 +1,14 @@
+package BackEnd;
+
+
+public class ListaProdutores {
+    
+    private final Produtor[] produtoresL;
+    private int numProd;
+
+    public ListaProdutores(int maxProd) {
+        produtoresL = new Produtor[maxProd];
+    }
+    
+    
+}
