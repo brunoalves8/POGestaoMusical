f7ef348b861447968ff81ed8db71007526cd5af5
kiko7792 <josebarreirinho92@gmail.com>
2022-11-25 15:07:27 +0000

    Criei a Lista de Musicas; Fiz umas pequenas alterações na class Musica;

--- ProjetoPO/src/BackEnd/ListaMusicas.java
+++ ProjetoPO/src/BackEnd/ListaMusicas.java
@@ -1,6 +1,46 @@
 package BackEnd;
 
+import java.util.Collection;
+import java.util.TreeSet;
 
 public class ListaMusicas {
-       
+
+    private Collection<Musica> musicas;
+
+    public ListaMusicas() {
+        musicas = new TreeSet<>();
+    }
+
+    public void adicionarMusica(Musica musica) {
+        musicas.add(musica);
+    }
+
+    public void removerMusica(int cod) {
+        for (Musica m : musicas) {
+            if (m.getCodigo() == cod) {
+                musicas.remove((Musica) m);
+            }
+        }
+    }
+
+    public Musica consultarDadosMusica(int cod) {
+        for (Musica m : musicas) {
+            if (m.getCodigo() == cod) {
+                return m;
+            }
+        }
+        System.out.println("Não existe nenhuma musica com esse codigo."); //não devemos ter prints dentro de metodos, que já fazem ações.
+        return null;
+    }
+
+    public void editarDadosMusica(int cod) {
+        for (Musica m : musicas) {
+            if (m.getCodigo() == cod) {
+                ((Musica) m).setTitulo(m.getTitulo());
+                ((Musica) m).setDuracao(m.getDuracao());
+                ((Musica) m).setArtistas(m.getArtistas());
+            }
+        }
+    }
+
 }
--- ProjetoPO/src/BackEnd/Musica.java
+++ ProjetoPO/src/BackEnd/Musica.java
@@ -2,17 +2,16 @@ package BackEnd;
 
 import java.util.ArrayList;
 
-
 public class Musica {
-    
+
     private String titulo;
     private double duracao;
+    private int codigo; //criei para ser possivel remover uma musica da lista de musicas
     private ArrayList<Musico> artistas = new ArrayList<>();
 
     public Musica() {
     }
 
-    
     public Musica(String titulo, double duracao) {
         this.titulo = titulo;
         this.duracao = duracao;
@@ -33,17 +32,38 @@ public class Musica {
     public void setDuracao(double duracao) {
         this.duracao = duracao;
     }
-    
-    public void adicionarMusico(Musico artist){
+
+    public void adicionarMusico(Musico artist) {
         artistas.add(artist);
     }
-    
-    public void removerMusico(int bi){
-        for(Musico m: artistas){
-            if(m.getBi() == bi){
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public ArrayList<Musico> getArtistas() {
+        return artistas;
+    }
+
+    public void setArtistas(ArrayList<Musico> artistas) {
+        this.artistas = artistas;
+    }
+
+    public void removerMusico(int bi) {
+        for (Musico m : artistas) {
+            if (m.getBi() == bi) {
                 artistas.remove(bi);
             }
         }
     }
-    
+
+    @Override
+    public String toString() {
+        return "Musica{" + "titulo=" + titulo + ", duracao=" + duracao + ", codigo=" + codigo + ", artistas=" + artistas + '}';
+    }
+
 }
