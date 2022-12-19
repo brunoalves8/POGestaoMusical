0e18ca0ae44096ee8c9ac9657fc6437d24d9a434
brunoalves8 <ba578731@gmail.com>
2022-12-19 13:03:38 +0000

    Update MainBruno.java

--- ProjetoPO/src/FrontEnd/MainBruno.java
+++ ProjetoPO/src/FrontEnd/MainBruno.java
@@ -1,14 +1,22 @@
 package FrontEnd;
 
+import java.util.Scanner;
+
 
 
 public class MainBruno {
 
     public static void main(String[] args) {
         
+        Scanner in = new Scanner(System.in);
+        
+        
         
         System.out.println("PAGINA INICIAL\n");
         System.out.println("Username: ");
+        String user = in.nextLine();
+        
+        String pass = in.nextLine();
     }
    
 }
